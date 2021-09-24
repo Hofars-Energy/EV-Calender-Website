6696182bdce7c645d78d504d3b5ff1d219a565d7
FireQueen-3010 <dikshapatro807@gmail.com>
2021-09-24 19:43:22 +0530

    Added

--- client/src/components/LoginBox.js
+++ client/src/components/LoginBox.js
@@ -1,7 +1,7 @@
 export default function Form() {
     return (
         <div className="login-box">
-            
+            <button className="btn"><a>Sign in with Google</a></button>
         </div>
     )
 }
