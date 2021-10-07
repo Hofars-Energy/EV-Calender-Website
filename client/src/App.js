04070c01a6ccb0308db2237972476119140e6746
FireQueen-3010 <dikshapatro807@gmail.com>
2021-10-07 21:03:14 +0530

    Added sign-up page custom login UIs

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,18 @@ import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Upcoming from './pages/Upcoming';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        {/* <Navbar/> */}
+        <Navbar/>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/upcoming" component={Upcoming} />
+          <Route exact path="/sign-up" component={SignUp} />
         </Switch>
       </Router>
     </div>
--- client/src/components/LoginBox.js
+++ client/src/components/LoginBox.js
@@ -1,7 +1,21 @@
+import Label from "./Label";
+import Input from "./Input";
+
 export default function Form() {
-    return (
-        <div className="login-box">
-            <button className="btn"><a>Sign in with Google</a></button>
-        </div>
-    )
+  return (
+    <div className="login-box">
+      <form className="login-box--form">
+        <Label for="login-email" text="Email"/>
+        <Input placeholder="Enter your email" type="email" id="login-email"/>
+        <Label for="login-password" text="Password" />
+        <Input placeholder="Enter your password" type="password" id="login-password"/>
+        <button className="btn"><a>Login</a></button>
+      </form>
+      <h3 className="login-box--or">(or)</h3>
+      <button className="btn">
+        <a>Sign in with Google</a>
+      </button>
+      <h4 className="login-box--end">New User? <a href="/sign-up">Sign up!</a></h4>
+    </div>
+  );
 }
